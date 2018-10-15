e398b637aa6846029ee45af220076c3b780e11a1
Paragon Initiative Enterprises <security@paragonie.com>
2018-10-15 16:26:16 -0400

    Add Test Vector from NIST SP 800-38A, F.5.5 CTR-AES256.Encrypt

--- tests/UtilTest.php
+++ tests/UtilTest.php
@@ -33,6 +33,56 @@ class UtilTest extends TestCase
     }
 
     /**
+     * @ref https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38a.pdf
+     *
+     * Key
+     * 603deb1015ca71be2b73aef0857d7781
+     * 1f352c073b6108d72d9810a30914dff4
+     *
+     * Init. Counter
+     * f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff
+     *
+     * Block #1
+     * Plaintext  6bc1bee22e409f96e93d7e117393172a
+     * Ciphertext 601ec313775789a5b7a7f504bbf3d228
+     *
+     * Block #2
+     * Plaintext  ae2d8a571e03ac9c9eb76fac45af8e51
+     * Ciphertext f443e3ca4d62b59aca84e990cacaf5c5
+     *
+     * Block #3
+     * Plaintext  30c81c46a35ce411e5fbc1191a0a52ef
+     * Ciphertext 2b0930daa23de94ce87017ba2d84988d
+     *
+     * Block #4
+     * Plaintext  f69f2445df4f9b17ad2b417be66c3710
+     * Ciphertext dfc9c58db67aada613c2dd08457941a6
+     *
+     */
+    public function testPolyfillAes256CtrTestVectors()
+    {
+        $key = Hex::decode('603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4');
+        $nonce = Hex::decode('f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff');
+        $plaintext = Hex::decode(
+            "6bc1bee22e409f96e93d7e117393172a" .
+            "ae2d8a571e03ac9c9eb76fac45af8e51" .
+            "30c81c46a35ce411e5fbc1191a0a52ef" .
+            "f69f2445df4f9b17ad2b417be66c3710"
+        );
+        $expected = "601ec313775789a5b7a7f504bbf3d228" .
+            "f443e3ca4d62b59aca84e990cacaf5c5" .
+            "2b0930daa23de94ce87017ba2d84988d" .
+            "dfc9c58db67aada613c2dd08457941a6";
+
+        $ciphertext = Util::aes256Ctr($plaintext, $key, $nonce);
+        $this->assertSame(
+            $expected,
+            Hex::encode($ciphertext),
+            'Test Vector from NIST SP 800-38A, F.5.5 CTR-AES256.Encrypt'
+        );
+    }
+
+    /**
      * @throws \SodiumException
      */
     public function testBitMask()
